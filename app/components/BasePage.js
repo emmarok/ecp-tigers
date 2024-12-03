eebe598ce8728f60ee79c07cbea90722cb166d4e
Stephen Collins <steve@Stephens-MacBook-Pro.local>
2024-12-03 09:20:38 +0000

    Charts

--- app/components/BasePage.js
+++ app/components/BasePage.js
@@ -1,6 +1,3 @@
-import DoughnutTwo from "../components/DoughnutTwo";
-import Drawer from "../components/Drawer";
-
 export default function Home () {
     return (<div className="bg-secondary h-full">
        <div className="container min-h-full max-w-full mx-auto px-16 py-16">
@@ -9,16 +6,14 @@ export default function Home () {
               <h1>We are LBG anything is within the realm of possibility</h1>
             </div>
             <div className="white_header_container">
-              <p className="text-black">Secondary motivational text</p>
-              <Drawer className/>
+              <p className="text-black">So long as you give 4 - 12 sprints and not a night before an event ... then we are fucked</p>
             </div>
             <div className="min-h-24"></div>
             <div className="green_container">
               <h1>We are LBG anything is within the realm of possibility</h1>
             </div>
             <div className="white_container">
-              <p className="text-black">Secondary motivational text</p>
-              <DoughnutTwo/>
+              <p className="text-black">So long as you give 4 - 12 sprints and not a night before an event ... then we are fucked</p>
             </div>
             
     </div>
